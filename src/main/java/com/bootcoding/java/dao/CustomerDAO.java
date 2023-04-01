17089bf0cd29f87c111fa53eb065dd07a7bf7841
Anshul Markwade <83603148+Anshul0707@users.noreply.github.com>
2023-04-01 12:06:14 +0530

    Update CustomerDAO.java

--- src/main/java/com/bootcoding/java/dao/CustomerDAO.java
+++ src/main/java/com/bootcoding/java/dao/CustomerDAO.java
@@ -13,8 +13,7 @@ public class CustomerDAO
 {
     private Connection conn;
 
-    public CustomerDAO()
-    {
+    public CustomerDAO(){
         DAOService daoService = new DAOService();
         conn = daoService.getDatabaseConnections();
     }
